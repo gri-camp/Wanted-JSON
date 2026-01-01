9de07236beb45360899adaeff42ab570302d7270
GRI-CAMP <gri-camp@yandex.ru>
2026-01-01 14:54:46 +0300

    5th_commit_auth_form_support

--- responses/responses.js
+++ responses/responses.js
@@ -936,7 +936,7 @@ const Responses = {
         "id": 222,
         "login": "foo"
     <strong class='purple'>}</strong>,
-    "accessToken": "your access-token (Base64)",    
+    "accessToken": "your accessToken (Base64)",    
 <strong class='purple'>}</strong>           
 </code>`,
     signup: `<code>    
@@ -945,7 +945,7 @@ const Responses = {
         "id": 222,
         "login": "foo"
     <strong class='purple'>}</strong>,
-    "accessToken": "your access-token (Base64)",    
+    "accessToken": "your accessToken (Base64)",    
 <strong class='purple'>}</strong>           
 </code>`,
     logout: ``,
@@ -955,8 +955,8 @@ const Responses = {
         "id": 13,
         "login": "test"
     <strong class='purple'>}</strong>,
-    "accessToken": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjEzLCJsb2dpbiI6InRlc3QiLCJpYXQiOjE3NjcwODY5MzcsImV4cCI6MTc2NzA4NzgzN30.zExmsjwwPl7J5HFJOCZpC8hQcXSjN2EPXUmdRD3nKr8",
-    "refreshToken": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjEzLCJsb2dpbiI6InRlc3QiLCJpYXQiOjE3NjcwODY5MzcsImV4cCI6MTc2NzY5MTczN30.t4fKrBjali5a9-XoQQJtaGTUbXQdiXjIX62j1ErOBqY",
+    "accessToken": "your accessToken (Base64)",
+    "refreshToken": "your refreshToken (Base64)",
 <strong class='purple'>}</strong>
 
 </code>`,
